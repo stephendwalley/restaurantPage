a4e2964f4d3a8e1624572ba31a8c3a08934d0565
Stephen Walley <stephendwalley@icloud.com>
2021-06-17 10:58:18 +1000

    page switches working

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,17 @@
 const menuPage = () => {
     // menu page module
-
+	const ELEMENT = document.getElementById("content");
     // Contact Us Display
-	const HEADER = document.getElementById("content")
+	const HEADER = document.createElement("h1")
 	HEADER.innerHTML = "Via Fratelli";
 
-	const PHONE_NUMBER = document.createElement("p");
-	PHONE_NUMBER.innerHTML = "0423 082 781";
+	const FOOD_ITEMS = document.createElement("p");
+	FOOD_ITEMS.innerHTML = "Margarita Pizza";
 
 	const EMAIL = document.createElement("p");
 	EMAIL.innerHTML = "contactus@viafratelli.com"
 
-	ELEMENT.append(HEADER, PHONE_NUMBER, EMAIL);
+	ELEMENT.append(HEADER, FOOD_ITEMS, EMAIL);
 
 }
 
